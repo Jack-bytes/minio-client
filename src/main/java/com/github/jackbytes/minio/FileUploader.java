c7c8d645c4a8c77b43d4073748ce47cfe82f7b64
Jackwang <78700393@qq.com>
2020-12-28 11:06:12 +0800

    minio-client

---
+++ src/main/java/com/github/jackbytes/minio/FileUploader.java
@@ -0,0 +1,34 @@
+package com.github.jackbytes.minio;
+
+import io.minio.*;
+import io.minio.errors.*;
+import io.minio.messages.Bucket;
+import io.minio.messages.NotificationConfiguration;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+public class FileUploader {
+
+    private static final MinioClient CLIENT;
+
+    static {
+        CLIENT = MinioClient.builder()
+                .endpoint("http://192.168.87.135:9000")
+                .credentials("jack", "wwh921314")
+                .build();
+    }
+
+    public static void uploadFile()
+            throws IOException, InvalidKeyException, InvalidResponseException,
+            InsufficientDataException, NoSuchAlgorithmException, ServerException,
+            InternalException, XmlParserException, ErrorResponseException {
+        NotificationConfiguration configuration = CLIENT.getBucketNotification(GetBucketNotificationArgs.builder().bucket("img").build());
+        boolean isExist = CLIENT.bucketExists(BucketExistsArgs.builder().bucket("img").build());
+        ObjectWriteResponse img = CLIENT.uploadObject(UploadObjectArgs.builder().bucket("img").object("test.jpg").filename("D:/其他/desktopBackground/2.png").build());
+        //client.listObjects();
+        List<Bucket> buckets = CLIENT.listBuckets();
+    }
+}
---
+++ src/test/java/com/github/jackbytes/minio/MinioTest.java
@@ -0,0 +1,22 @@
+package com.github.jackbytes.minio;
+
+import io.minio.errors.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class MinioTest {
+
+    @Test
+    public void test1() throws IOException, InvalidResponseException, InvalidKeyException, NoSuchAlgorithmException, ServerException, ErrorResponseException, XmlParserException, InsufficientDataException, InternalException {
+
+        FileUploader.uploadFile();
+    }
+
+    @Test
+    public void test2(){
+
+    }
+}
